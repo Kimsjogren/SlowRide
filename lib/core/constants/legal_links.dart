1e07ef227754de83682833353cc1cd29ac91ff53
Kim <kimsjogren87@gmail.com>
2026-07-14 02:44:54 +0200

    Fix in-app support link

--- lib/core/constants/legal_links.dart
+++ lib/core/constants/legal_links.dart
@@ -3,5 +3,5 @@ class LegalLinks {
 
   static const String privacyPolicy = 'https://cruizx.com/privacy?lang=en';
   static const String termsOfUse = 'https://cruizx.com/terms?lang=en';
-  static const String support = 'https://cruizx.com/support?lang=en';
+  static const String support = 'https://cruizx.com/support';
 }
